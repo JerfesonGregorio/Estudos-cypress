9cbac136e0c6defe23bc2e0180d2fac8f016b7a7
JerfesonGregorio <pro.jerfeson@gmail.com>
2024-02-23 09:44:13 -0300

    testando requisicao de uma api

--- cypress/e2e/teste-api.cy.js
+++ cypress/e2e/teste-api.cy.js
@@ -1,5 +1,5 @@
 describe('testando a API', () => {
-    it('Dados da API', () => {
+    /* it('Dados da API', () => {
         cy.request({
             method: 'POST',
             url: 'http://localhost:3000/user/login',
@@ -9,6 +9,19 @@ describe('testando a API', () => {
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('id')
             expect(res.body.id).to.be.equal(5)
+        })
+    }); */
+
+    it('Buscando dados da API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:3000/teste/photos',
+            body: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body[0]).to.have.property('description')
+            expect(res.body[0].description).to.be.equal('Lobo')
         })
     });
 });
